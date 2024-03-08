0dd9152bf8c6882715b91eac6542c18d373245d1
Budik1 <vl.ananin@gmail.com>
2024-03-08 21:16:46 +0600

    first

---
+++ fun.py
@@ -0,0 +1,24 @@
+import pyautogui
+from time import sleep
+
+
+def move_to_click(pos_click: tuple, z_p_k: float):
+    """
+    Поместить указатель мыши по координатам и кликнуть, учитывая задержку.
+    :param pos_click: Point
+    :param z_p_k: задержка перед кликом(float)
+    :return: None
+    """
+    # print('move_to_click', pos_click)
+    sleep(0.3)
+    pyautogui.moveTo(pos_click, duration=1, tween=pyautogui.easeInOutQuad)
+    # print('должен быть клик')
+    sleep(z_p_k)
+    pyautogui.click(pos_click)
+    sleep(0.18)
+
+
+def push_close():
+    print('def "fun.push_close"')
+    close = pyautogui.locateCenterOnScreen('img/close.png', confidence=0.9)
+    move_to_click(close, 0.1)
---
+++ main.py
@@ -0,0 +1,59 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk
+import fun
+
+
+root = Tk()
+
+root.title(' помощник "Метро 2033"')
+root.geometry("327x380+1240+50")  # Ширина x Высота + координата X + координата Y
+root.resizable(False, False)
+
+# status_kiki = IntVar(value=0)
+# status_krysa = StringVar(value='0')
+# status_vip = StringVar(value='0')
+#
+# ttk.Button(text=" Start ", width=13, command=fun.start_p_m).place(x=0, y=0)
+# ttk.Button(text=" сбор бонуса ", width=13, command=fun.bonus, state="disabled").place(x=0, y=32)
+# ttk.Label(text=status_bonus).place(x=130, y=32)
+# ttk.Button(text="  сбор подарков  ", width=13, command=fun.station_gifts, state="disabled").place(x=0, y=64)
+# ttk.Button(text=" обход VIP ", width=13, command=tent_inspection).place(x=0, y=96)
+# ttk.Label(textvariable=status_vip).place(x=130, y=96)
+# # шаг 31
+# ttk.Button(text=" на Киевскую ", width=11, command=touring.frunze_kiev).place(x=120, y=169)
+# ttk.Button(text=" домой ", width=11, command=touring.kiev_frunze).place(x=120, y=200)
+# ttk.Button(text="кикиморы", width=11, command=touring.za_kikimorami).place(x=153, y=31)
+# ttk.Label(textvariable=status_kiki, background="#FFCDD2", foreground="#B71C1C", padding=4).place(x=263, y=31)
+# ttk.Button(text="Паук + Ящер", width=11, command=touring.pauk_yascher).place(x=153, y=0)
+#
+# ttk.Button(text="test гардероб", width=11, command=person.pereodevanie).place(x=109, y=245)
+#
+# ttk.Button(text="most_frunze", width=11, command=touring.most_frunze).place(x=218, y=277)
+# ttk.Button(text="frunze_most", width=11, command=touring.frunze_most).place(x=218, y=308)
+#
+# ttk.Button(text="most_riga", width=11, command=touring.most_riga).place(x=109, y=277)  # x=133
+# ttk.Button(text="riga_most", width=11, command=touring.riga_most).place(x=109, y=308)
+#
+# ttk.Button(text="frunze_riga", width=11, command=touring.frunze_riga).place(x=0, y=277)
+# ttk.Button(text="riga_frunze", width=11, command=touring.riga_frunze).place(x=0, y=308)
+#
+# ttk.Button(text="задания на Киевской", width=17, command=touring.tasks_na_kievskoy).place(x=120, y=138)
+# # тест пробежка
+# ttk.Button(text="тест пробежка", width=13, command=dvizh_test).place(x=153, y=64)
+# ttk.Label(textvariable=status_krysa, background="#FFCDD2", foreground="#0000FF", padding=4).place(x=285, y=64)
+# ttk.Button(text="обход всех станций", width=16, command=touring.sbor_podarkov).place(x=153, y=96)
+#
+# imagePul = ImageTk.PhotoImage(file="img/pulya.png")
+# ttk.Button(root, image=imagePul, command=station_master.vybor_zadaniya_na_puli).place(x=60, y=145)
+#
+# img_e1 = ImageTk.PhotoImage(file="img/en1v3.png")
+# ttk.Button(root, image=img_e1, command=en_1).place(x=0, y=128)
+# #
+# img_e2 = ImageTk.PhotoImage(file="img/en2v3.png")
+# ttk.Button(root, image=img_e2, command=en_2).place(x=0, y=168)
+# #
+# img_e3 = ImageTk.PhotoImage(file="img/en3v3.png")
+# ttk.Button(root, image=img_e3, command=en_3).place(x=0, y=208)
+
+root.mainloop()
